f19f9804ae30e12937200fd08626ffca64ccd8c3
jenellep <jenellep@student.21-school.ru>
2023-03-27 00:50:19 +0300

    Some urls, GPT generated html for test

--- Chess21_main/main_pages/views.py
+++ Chess21_main/main_pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'main_pages/main_page.html')
+
+
+def about(request):
+    return render(request, 'main_pages/about_page.html')
+
+
+def chess(request):
+    return render(request, 'main_pages/chess.html')
